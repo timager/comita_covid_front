d491eb6f9defc4dbcd5eefe6adb97afad801916b
timager <timger98@gmail.com>
2020-03-26 16:20:43 +0300

    buttons

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,6 +1,7 @@
 import React, {Component} from "react";
 import "./../assets/css/LoginPage.css";
 import Button from "./Button";
+import axios from "axios";
 import Input from "./Input";
 
 class LoginPage extends Component {
@@ -12,18 +13,29 @@ class LoginPage extends Component {
                     <p className={"f_norm"}>Brainstorming</p>
                     <p className={"f_large f_bold"}>Преодоление COVID-19</p>
                     <p className={"f_large f_bold"}>для индустрии транспорта</p>
-                    <Button className={"conf_button"}>Программа конференции</Button>
+                    <a target={"_blank"} href={"http://live.covid-transport.ru/symfony/public/Covid-2020-rus.pdf"}><Button className={"conf_button"}>Программа конференции</Button></a>
                 </div>
                 <div className={"login_form"}>
                     <p className={"f_large f_bold"}>Добро пожаловать в прямой эфир</p>
                     <div>
-                        <Input type={"text"} placeholder={"логин"}/>
-                        <Input type={"password"} placeholder={"пароль"}/>
-                        <Button>Войти</Button>
+                        <Input type={"text"} placeholder={"логин"} onChange={e => {
+                            this.setState({login: e.target.value})
+                        }}/>
+                        <Input type={"password"} placeholder={"пароль"} onChange={e => {
+                            this.setState({pass: e.target.value})
+                        }}/>
+                        <Button onClick={this.loginClick}>Войти</Button>
                     </div>
                 </div>
             </div>
         )
+    }
+
+    loginClick() {
+        axios.post(axios.defaults.baseURL + "/api/login", {login: this.state.login, pass: this.state.pass})
+            .then(res => {
+                console.log(res)
+            })
     }
 }
 
