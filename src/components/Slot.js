56881f0955fd86415a09bafd4c5839a4d96d634a
timager <timger98@gmail.com>
2020-03-26 22:55:10 +0300

    meet containers

--- src/components/Slot.js
+++ src/components/Slot.js
@@ -4,13 +4,38 @@ import "./../assets/css/Slot.css";
 class Slot extends Component {
     render() {
         let slot = this.props.slot;
+        console.log(slot);
+        let slotBody = <div className={"available"}/>;
+        if (!slot['is_actual']) {
+            if (slot.meets.length > 0) {
+                slotBody =
+                    <div className={slot.meets.length >= 10 ? "closed" : "available"}>
+                        {slot.meets.length + " из 10"}
+                    </div>;
+                let mine = false;
+                slot.meets.forEach(meet => {
+                    if (/*meet['guest']['id'] === this.props.currentUser['id']*/true) {
+                        mine = true;
+                    }
+                });
+                if (mine) {
+                    slotBody =
+                        <div className={"mine"}>
+                            {slot.meets.length + " из 10"}
+                        </div>
+                }
+            }
+        } else {
+            slotBody = <div className={"timeout"}/>;
+        }
+
         return (
             <div className={"slot"}>
                 <div className={"slot_header"}>
                     <div>{slot.time}</div>
                 </div>
                 <div className={"slot_body"}>
-
+                    {slotBody}
                 </div>
             </div>
         );
