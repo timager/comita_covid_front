aa6a5c253b59e0c70da45001a472fd9441f6b46a
timager <timger98@gmail.com>
2020-03-27 20:57:12 +0300

    recovery pass button

--- src/components/UserPreview.js
+++ src/components/UserPreview.js
@@ -1,6 +1,6 @@
 import React, {Component} from "react";
 import "./../assets/css/UserPreview.css";
-import {faPhone, faEdit, faTrash} from "@fortawesome/free-solid-svg-icons";
+import {faPhone, faEdit, faTrash, faKey} from "@fortawesome/free-solid-svg-icons";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import axios from "axios";
 import {BASE_URL} from "../const";
@@ -13,6 +13,7 @@ class UserPreview extends Component {
     constructor(props) {
         super(props);
         this.deleteHandler = this.deleteHandler.bind(this);
+        this.recoverPassword = this.recoverPassword.bind(this);
     }
 
     render() {
@@ -32,6 +33,7 @@ class UserPreview extends Component {
                                      icon={faEdit}/>
                     <br/>
                     <FontAwesomeIcon onClick={this.deleteHandler} className={"min_btn red"} icon={faTrash}/>
+                    <FontAwesomeIcon onClick={this.recoverPassword} className={"min_btn blue"} icon={faKey}/>
                 </div>
             </div>
         );
@@ -63,6 +65,36 @@ class UserPreview extends Component {
             ]
         });
     }
+
+    recoverPassword() {
+        confirmAlert({
+            title: 'Восстановление',
+            message: 'Вы уверены что хотите сгенерировать и отправить на почту пользователя новый пароль?',
+            buttons: [
+                {
+                    label: 'Да',
+                    onClick: () => axios.post(BASE_URL + "/api/users/" + this.props.user.id + "/recovery")
+                        .then(res => this.props.loadUsers()).catch(
+                            e => confirmAlert({
+                                title: 'Ошибка',
+                                message: e.response.data,
+                                buttons: [
+                                    {
+                                        label: 'Ок'
+                                    }
+                                ]
+                            })
+                        )
+                },
+                {
+                    label: 'Отмена'
+                }
+            ]
+        });
+    }
+
+
+
 
 }
 
